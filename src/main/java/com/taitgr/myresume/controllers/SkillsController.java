55244574b7ddb8fef88317250fa2b16f32505185
gatait <gabe.tait@sas.com>
2020-08-02 15:21:24 -0400

    Update To Use Service

--- src/main/java/com/taitgr/myresume/controllers/SkillsController.java
+++ src/main/java/com/taitgr/myresume/controllers/SkillsController.java
@@ -2,6 +2,7 @@ package com.taitgr.myresume.controllers;
 
 import com.taitgr.myresume.dao.SkillDaoImpl;
 import com.taitgr.myresume.models.Skill;
+import com.taitgr.myresume.service.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +21,30 @@ import java.util.List;
 public class SkillsController {
 
     @Autowired
-    private SkillDaoImpl skillDaoImpl;
+    private SkillService skillService;
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Skill> getAllSkills() {
-        return skillDaoImpl.getAllSkills();
+        return skillService.getAllSkills();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Skill getSkill(@PathVariable("id") Long id) {
-        return skillDaoImpl.getSkill(id);
+        return skillService.getSkillById(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createSkill(@RequestBody Skill skill) {
-        skillDaoImpl.createSkill(skill);
+        skillService.create(skill);
     }
 
     @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updateSkill(@RequestBody Skill skill) {
-        skillDaoImpl.updateSkill(skill);
+        skillService.updateSkill(skill);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteSkill(@PathVariable("id") Long id) {
-        skillDaoImpl.deleteSkill(id);
+        skillService.deleteSkill(id);
     }
 }
--- src/main/java/com/taitgr/myresume/service/SkillService.java
+++ src/main/java/com/taitgr/myresume/service/SkillService.java
@@ -1,9 +1,13 @@
 package com.taitgr.myresume.service;
 
 import com.taitgr.myresume.dao.SkillDao;
+import com.taitgr.myresume.dao.SkillDaoImpl;
+import com.taitgr.myresume.models.Skill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * SkillService
@@ -16,5 +20,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class SkillService {
 
-    private SkillDao skillDao;
+    @Autowired
+    private SkillDaoImpl skillDaoImpl;
+
+    public void create(Skill skill) {
+        skillDaoImpl.createSkill(skill);
+    }
+
+    public List<Skill> getAllSkills() {
+        return skillDaoImpl.getAllSkills();
+    }
+
+    public Skill getSkillById(Long id) {
+        return skillDaoImpl.getSkill(id);
+    }
+
+    public void updateSkill(Skill skill) {
+        skillDaoImpl.updateSkill(skill);
+    }
+
+    public void deleteSkill(Long id) {
+        skillDaoImpl.deleteSkill(id);
+    }
 }
